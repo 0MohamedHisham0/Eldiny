5acb7dcc20799ff86104228728b3212d37668986
0MohamedHisham0 <MohamedDal4300@gmail.com>
2022-03-13 12:56:50 +0200

    add qurans Screens

---
+++ lib/modules/el-quran/resume-quran/resume-quran.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Resume_Quran extends StatelessWidget {
+  const Resume_Quran({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/modules/el-quran/surah-quran/surah-quran.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Surah_Quran extends StatelessWidget {
+  const Surah_Quran({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
